ee64d9eb8cb1e8caf268bd769910a3d077a946d6
Ben Merchant <benjaminmerchant@gmail.com>
2021-12-29 12:04:59 -0500

    fix minor code formatting in last commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [path, setPath] = useState('My First Path')
+  const [stops, setStops] = useState([
+    {
+      id: 1,
+      name: 'Home',
+      time: 0
+    },
+    {
+      id: 2,
+      name: 'Stairs',
+      time: 0
+    },
+    {
+      id: 3,
+      name: 'Natchez',
+      time: 0
+    },
+    {
+      id: 4,
+      name: 'Virginia',
+      time: 0
+    },
+    {
+      id: 5,
+      name: 'Grandview',
+      time: 0
+    }
+  ])
+  const [currentTime, setCurrentTime] = useState(938436)
+
   return (
     <div className="App">
-      <div className="AppContainer">
-        <div className="ContainerHeader">
+      <div className="AppContainer SimpleBorder">
+        <div className="ContainerHeader SimpleBorder">
           Pittsburgh Paths
         </div>
-        <div className="ClockArea">
-          <div className="ClockActual">
-            { Date.now() }
+        <div className="ClockArea SimpleBorder">
+          <div className="ClockActual SimpleBorder">
+            { Math.trunc((currentTime / 1000) / 60)  }
+            :
+            { currentTime / 1000 }
           </div>
           <div className="ClockButtons">
             <button>Start</button>
             <button>Split</button>
           </div>
-          <div className="PathArea">
-            <div className="PathHeader">
-              Path Name
-            </div>
-            <div className="PathList">
-              <ul>Stop 1</ul>
-              <ul>Stop 2</ul>
-              <ul>Stop 3</ul>
-              <ul>Stop 4</ul>
-              <ul>Stop 5</ul>
+          <div className="PathArea SimpleBorder">
+            <h3 className="PathHeader">
+              {path}
+            </h3>
+            <div className="PathList">                
+                  <ul 
+                    
+                  >{stops.map(stop =>
+                      <li key={stop.id}
+                    className="SimpleBorder PathList">
+                        <p>{stop.name}</p>
+                      </li>
+                  )}
+                  </ul>
+                
+              
             </div>
           </div>
         </div>
